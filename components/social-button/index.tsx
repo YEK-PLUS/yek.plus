a9208d9eca421dc020b1db4076863334e9752dd6
Yunus Emre Köker <yunusemrekokerr@gmail.com>
2022-04-01 02:56:15 +0300

    add ref to SocialButton

--- components/social-button/index.tsx
+++ components/social-button/index.tsx
@@ -1,23 +1,37 @@
 import { IconType } from '@react-icons/all-files';
+import { forwardRef, ForwardedRef } from 'react';
 
-const SocialButton = ({
-  title,
-  icon: Icon,
-  href,
-  target,
-}: {
-  icon: IconType;
-  title: string;
-  href?: string;
-  target?: string;
-}) => (
-  <div className="flex flex-row items-center">
-    <Icon className="mr-2" />
-    <a href={href} target={target} className="underline transition-all hover:decoration-light-red" rel="noreferrer">
-      {title}
-    </a>
-  </div>
+const SocialButton = forwardRef(
+  (
+    {
+      icon: Icon,
+      title,
+      href,
+      target,
+    }: {
+      icon: IconType;
+      title: string;
+      href?: string;
+      target?: string;
+    },
+    ref: ForwardedRef<HTMLAnchorElement>,
+  ) => (
+    <div className="flex flex-row items-center">
+      <Icon className="mr-2" />
+      <a
+        ref={ref}
+        href={href}
+        target={target}
+        className="underline transition-all hover:decoration-light-red"
+        rel="noreferrer"
+      >
+        {title}
+      </a>
+    </div>
+  ),
 );
+
+SocialButton.displayName = 'SocialButton';
 SocialButton.defaultProps = {
   href: '#',
   target: '_blank',
