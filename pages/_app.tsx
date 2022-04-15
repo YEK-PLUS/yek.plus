60fbc1e3a1be27d7e2c9877dcfde04c8ed9356dd
Yunus Emre Köker <yunusemrekokerr@gmail.com>
2022-04-15 12:54:42 +0300

    fix full screen bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import LogRocketHook from 'hooks/logrocket';
 const MyApp = ({ Component, pageProps }: AppProps) => {
   LogRocketHook();
   return (
-    <div className="w-full h-screen mx-auto flex justify-center items-center bg-gray-100 dark:bg-fancy-gray">
+    <div className="w-full h-full mx-auto flex justify-center items-center bg-gray-100 dark:bg-fancy-gray">
       <div className="w-full max-w-screen-md bg-white/5 p-4 sm:p-10 mx-4">
         <Component {...pageProps} />
       </div>
