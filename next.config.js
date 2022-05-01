b6977a6ff0aa2e5dd52129c7e4a6c30b3cceaa5b
Yunus Emre Köker <yunusemrekokerr@gmail.com>
2022-05-02 01:59:27 +0300

    add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const { NEXT_PUBLIC_STRAPI_ENDPOINT } = process.env;
-const nextConfig = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   images: {
     domains: [new URL(NEXT_PUBLIC_STRAPI_ENDPOINT).hostname],
   },
-};
-
-module.exports = nextConfig;
+});
