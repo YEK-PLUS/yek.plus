88d2b8700d661bb66e12f44cc410743bb12a8fd8
Yunus Emre Köker <yunusemrekokerr@gmail.com>
2022-05-20 18:05:18 +0300

    update layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,8 +6,8 @@ import LogRocketHook from 'hooks/logrocket';
 const MyApp = ({ Component, pageProps }: AppProps) => {
   LogRocketHook();
   return (
-    <div className="w-full min-h-full mx-auto flex justify-center items-center bg-gray-100 dark:bg-fancy-gray p-4">
-      <div className="w-full max-w-screen-md bg-white/5 p-4 sm:p-10">
+    <div className="w-full min-h-full mx-auto flex justify-center bg-gray-100 dark:bg-fancy-gray p-4 py-4">
+      <div className="w-full max-w-screen-md h-min bg-white/5 p-4 sm:p-10">
         <Component {...pageProps} />
       </div>
     </div>
