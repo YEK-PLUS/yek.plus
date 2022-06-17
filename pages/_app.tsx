7225ed3778691f089bd23ab8c9df1e9b8b2bec01
Yunus Emre <yunusemrekokerr@gmail.com>
2022-06-17 17:34:33 +0000

    setup google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,28 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import LogRocketHook from 'hooks/logrocket';
+import Script from 'next/script';
+
+const { NEXT_PUBLIC_GUA } = process.env;
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   LogRocketHook();
   return (
     <div className="w-full min-h-full mx-auto flex justify-center bg-gray-100 dark:bg-fancy-gray p-4 py-4">
       <div className="w-full max-w-screen-md h-min bg-white/5 p-4 sm:p-10">
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${NEXT_PUBLIC_GUA}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${NEXT_PUBLIC_GUA}');
+        `}
+        </Script>
         <Component {...pageProps} />
       </div>
     </div>
